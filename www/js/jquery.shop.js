3df75cfb5fa85e0560f7c5ee76540bb093060a2a
patriciabueno <patibueno82@gmail.com>
2015-05-10 13:58:28 +0100

    MyOwnJQueryShop

--- www/js/jquery.shop.js
+++ www/js/jquery.shop.js
@@ -9,7 +9,6 @@
 		
 		    // Properties
 		
-<<<<<<< HEAD
 			this.counterPrefix = "winery-"; // Prefix string to be prepended to the counter's name in the session storage
 			this.counterName = this.counterPrefix + "counter"; // counter name in the session storage
 			this.shippingRates = this.counterPrefix + "shipping-rates"; // Shipping rates key in the session storage
@@ -26,24 +25,6 @@
 			this.$caloriecounterActions = this.$element.find( "#calorie-counter-actions" ); // counter actions links
 			this.$updatecounterBtn = this.$caloriecounterActions.find( "#update-counter" ); // Update counter button
 			this.$emptycounterBtn = this.$caloriecounterActions.find( "#empty-counter" ); // Empty counter button
-=======
-			this.cartPrefix = "winery-"; // Prefix string to be prepended to the cart's name in the session storage
-			this.cartName = this.cartPrefix + "cart"; // Cart name in the session storage
-			this.shippingRates = this.cartPrefix + "shipping-rates"; // Shipping rates key in the session storage
-			this.total = this.cartPrefix + "total"; // Total key in the session storage
-			this.storage = sessionStorage; // shortcut to the sessionStorage object
-			
-			
-			this.$formAddToCart = this.$element.find( "form.add-to-cart" ); // Forms for adding items to the cart
-			this.$formCart = this.$element.find( "#shopping-cart" ); // Shopping cart form
-			this.$checkoutCart = this.$element.find( "#checkout-cart" ); // Checkout form cart
-			this.$checkoutOrderForm = this.$element.find( "#checkout-order-form" ); // Checkout user details form
-			this.$shipping = this.$element.find( "#sshipping" ); // Element that displays the shipping rates
-			this.$subTotal = this.$element.find( "#stotal" ); // Element that displays the subtotal charges
-			this.$shoppingCartActions = this.$element.find( "#shopping-cart-actions" ); // Cart actions links
-			this.$updateCartBtn = this.$shoppingCartActions.find( "#update-cart" ); // Update cart button
-			this.$emptyCartBtn = this.$shoppingCartActions.find( "#empty-cart" ); // Empty cart button
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 			this.$userDetails = this.$element.find( "#user-details-content" ); // Element that displays the user information
 			this.$paypalForm = this.$element.find( "#paypal-form" ); // PayPal form
 			
@@ -68,21 +49,12 @@
 			
 			// Method invocation
 			
-<<<<<<< HEAD
 			this.createcounter();
 			this.handleAddTocounterForm();
 			this.handleCheckoutOrderForm();
 			this.emptycounter();
 			this.updatecounter();
 			this.displaycounter();
-=======
-			this.createCart();
-			this.handleAddToCartForm();
-			this.handleCheckoutOrderForm();
-			this.emptyCart();
-			this.updateCart();
-			this.displayCart();
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 			this.displayUserDetails();
 			this.populatePayPalForm();
 			
@@ -91,7 +63,6 @@
 		
 		// Public methods
 		
-<<<<<<< HEAD
 		// Creates the counter keys in the session storage
 		
 		createcounter: function() {
@@ -101,17 +72,6 @@
 				counter.items = [];
 			
 				this.storage.setItem( this.counterName, this._toJSONString( counter ) );
-=======
-		// Creates the cart keys in the session storage
-		
-		createCart: function() {
-			if( this.storage.getItem( this.cartName ) == null ) {
-			
-				var cart = {};
-				cart.items = [];
-			
-				this.storage.setItem( this.cartName, this._toJSONString( cart ) );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				this.storage.setItem( this.shippingRates, "0" );
 				this.storage.setItem( this.total, "0" );
 			}
@@ -123,39 +83,22 @@
 			var self = this;
 			if( self.$paypalForm.length ) {
 				var $form = self.$paypalForm;
-<<<<<<< HEAD
 				var counter = self._toJSONObject( self.storage.getItem( self.counterName ) );
 				var shipping = self.storage.getItem( self.shippingRates );
 				var numShipping = self._convertString( shipping );
 				var counterItems = counter.items; 
 				var singShipping = Math.floor( numShipping / counterItems.length );
-=======
-				var cart = self._toJSONObject( self.storage.getItem( self.cartName ) );
-				var shipping = self.storage.getItem( self.shippingRates );
-				var numShipping = self._convertString( shipping );
-				var cartItems = cart.items; 
-				var singShipping = Math.floor( numShipping / cartItems.length );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				
 				$form.attr( "action", self.paypalURL );
 				$form.find( "input[name='business']" ).val( self.paypalBusinessEmail );
 				$form.find( "input[name='currency_code']" ).val( self.paypalCurrency );
 				
-<<<<<<< HEAD
 				for( var i = 0; i < counterItems.length; ++i ) {
 					var counterItem = counterItems[i];
 					var n = i + 1;
 					var name = counterItem.product;
 					var kcals = counterItem.kcals;
 					var qty = counterItem.qty;
-=======
-				for( var i = 0; i < cartItems.length; ++i ) {
-					var cartItem = cartItems[i];
-					var n = i + 1;
-					var name = cartItem.product;
-					var price = cartItem.price;
-					var qty = cartItem.qty;
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 					
 					$( "<div/>" ).html( "<input type='hidden' name='quantity_" + n + "' value='" + qty + "'/>" ).
 					insertBefore( "#paypal-btn" );
@@ -163,11 +106,7 @@
 					insertBefore( "#paypal-btn" );
 					$( "<div/>" ).html( "<input type='hidden' name='item_number_" + n + "' value='SKU " + name + "'/>" ).
 					insertBefore( "#paypal-btn" );
-<<<<<<< HEAD
 					$( "<div/>" ).html( "<input type='hidden' name='amount_" + n + "' value='" + self._formatNumber( kcals, 2 ) + "'/>" ).
-=======
-					$( "<div/>" ).html( "<input type='hidden' name='amount_" + n + "' value='" + self._formatNumber( price, 2 ) + "'/>" ).
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 					insertBefore( "#paypal-btn" );
 					$( "<div/>" ).html( "<input type='hidden' name='shipping_" + n + "' value='" + self._formatNumber( singShipping, 2 ) + "'/>" ).
 					insertBefore( "#paypal-btn" );
@@ -246,7 +185,6 @@
 			}
 		},
 		
-<<<<<<< HEAD
 		// Displays the calorie counter
 		
 		displaycounter: function() {
@@ -255,22 +193,11 @@
 				var items = counter.items;
 				var $tablecounter = this.$formcounter.find( ".calorie-counter" );
 				var $tablecounterBody = $tablecounter.find( "tbody" );
-=======
-		// Displays the shopping cart
-		
-		displayCart: function() {
-			if( this.$formCart.length ) {
-				var cart = this._toJSONObject( this.storage.getItem( this.cartName ) );
-				var items = cart.items;
-				var $tableCart = this.$formCart.find( ".shopping-cart" );
-				var $tableCartBody = $tableCart.find( "tbody" );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				
 				
 				for( var i = 0; i < items.length; ++i ) {
 					var item = items[i];
 					var product = item.product;
-<<<<<<< HEAD
 					var kcals = this.currency + " " + item.kcals;
 					var qty = item.qty;
 					var html = "<tr><td class='pname'>" + product + "</td>" + "<td class='pqty'><input type='text' value='" + qty + "' class='qty'/></td>" + "<td class='pkcals'>" + kcals + "</td></tr>";
@@ -301,43 +228,10 @@
 				
 				this.$subTotal[0].innerHTML = this.currency + " " + this._convertNumber( subTot );
 				this.$shipping[0].innerHTML = this.currency + " " + counterShipping;
-=======
-					var price = this.currency + " " + item.price;
-					var qty = item.qty;
-					var html = "<tr><td class='pname'>" + product + "</td>" + "<td class='pqty'><input type='text' value='" + qty + "' class='qty'/></td>" + "<td class='pprice'>" + price + "</td></tr>";
-					
-					$tableCartBody.html( $tableCartBody.html() + html );
-				}
-				
-				var total = this.storage.getItem( this.total );
-				this.$subTotal[0].innerHTML = this.currency + " " + total;
-			} else if( this.$checkoutCart.length ) {
-				var checkoutCart = this._toJSONObject( this.storage.getItem( this.cartName ) );
-				var cartItems = checkoutCart.items;
-				var $cartBody = this.$checkoutCart.find( "tbody" );
-				
-				for( var j = 0; j < cartItems.length; ++j ) {
-					var cartItem = cartItems[j];
-					var cartProduct = cartItem.product;
-					var cartPrice = this.currency + " " + cartItem.price;
-					var cartQty = cartItem.qty;
-					var cartHTML = "<tr><td class='pname'>" + cartProduct + "</td>" + "<td class='pqty'>" + cartQty + "</td>" + "<td class='pprice'>" + cartPrice + "</td></tr>";
-					
-					$cartBody.html( $cartBody.html() + cartHTML );
-				}
-				
-				var cartTotal = this.storage.getItem( this.total );
-				var cartShipping = this.storage.getItem( this.shippingRates );
-				var subTot = this._convertString( cartTotal ) + this._convertString( cartShipping );
-				
-				this.$subTotal[0].innerHTML = this.currency + " " + this._convertNumber( subTot );
-				this.$shipping[0].innerHTML = this.currency + " " + cartShipping;
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 			
 			}
 		},
 		
-<<<<<<< HEAD
 		// Empties the counter by calling the _emptycounter() method
 		// @see $.Shop._emptycounter()
 		
@@ -346,21 +240,10 @@
 			if( self.$emptycounterBtn.length ) {
 				self.$emptycounterBtn.on( "click", function() {
 					self._emptycounter();
-=======
-		// Empties the cart by calling the _emptyCart() method
-		// @see $.Shop._emptyCart()
-		
-		emptyCart: function() {
-			var self = this;
-			if( self.$emptyCartBtn.length ) {
-				self.$emptyCartBtn.on( "click", function() {
-					self._emptyCart();
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				});
 			}
 		},
 		
-<<<<<<< HEAD
 		// Updates the counter
 		
 		updatecounter: function() {
@@ -369,34 +252,18 @@
 			self.$updatecounterBtn.on( "click", function() {
 				var $rows = self.$formcounter.find( "tbody tr" );
 				var counter = self.storage.getItem( self.counterName );
-=======
-		// Updates the cart
-		
-		updateCart: function() {
-			var self = this;
-		  if( self.$updateCartBtn.length ) {
-			self.$updateCartBtn.on( "click", function() {
-				var $rows = self.$formCart.find( "tbody tr" );
-				var cart = self.storage.getItem( self.cartName );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				var shippingRates = self.storage.getItem( self.shippingRates );
 				var total = self.storage.getItem( self.total );
 				
 				var updatedTotal = 0;
 				var totalQty = 0;
-<<<<<<< HEAD
 				var updatedcounter = {};
 				updatedcounter.items = [];
-=======
-				var updatedCart = {};
-				updatedCart.items = [];
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				
 				$rows.each(function() {
 					var $row = $( this );
 					var pname = $.trim( $row.find( ".pname" ).text() );
 					var pqty = self._convertString( $row.find( ".pqty > .qty" ).val() );
-<<<<<<< HEAD
 					var pkcals = self._convertString( self._extractkcals( $row.find( ".pkcals" ) ) );
 					
 					var counterObj = {
@@ -408,36 +275,18 @@
 					updatedcounter.items.push( counterObj );
 					
 					var subTotal = pqty * pkcals;
-=======
-					var pprice = self._convertString( self._extractPrice( $row.find( ".pprice" ) ) );
-					
-					var cartObj = {
-						product: pname,
-						price: pprice,
-						qty: pqty
-					};
-					
-					updatedCart.items.push( cartObj );
-					
-					var subTotal = pqty * pprice;
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 					updatedTotal += subTotal;
 					totalQty += pqty;
 				});
 				
 				self.storage.setItem( self.total, self._convertNumber( updatedTotal ) );
 				self.storage.setItem( self.shippingRates, self._convertNumber( self._calculateShipping( totalQty ) ) );
-<<<<<<< HEAD
 				self.storage.setItem( self.counterName, self._toJSONString( updatedcounter ) );
-=======
-				self.storage.setItem( self.cartName, self._toJSONString( updatedCart ) );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				
 			});
 		  }
 		},
 		
-<<<<<<< HEAD
 		// Adds items to the calorie counter
 		
 		handleAddTocounterForm: function() {
@@ -446,21 +295,10 @@
 				var $form = $( this );
 				var $product = $form.parent();
 				var kcals = self._convertString( $product.data( "kcals" ) );
-=======
-		// Adds items to the shopping cart
-		
-		handleAddToCartForm: function() {
-			var self = this;
-			self.$formAddToCart.each(function() {
-				var $form = $( this );
-				var $product = $form.parent();
-				var price = self._convertString( $product.data( "price" ) );
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 				var name =  $product.data( "name" );
 				
 				$form.on( "submit", function() {
 					var qty = self._convertString( $form.find( ".qty" ).val() );
-<<<<<<< HEAD
 					var subTotal = qty * kcals;
 					var total = self._convertString( self.storage.getItem( self.total ) );
 					var sTotal = total + subTotal;
@@ -468,15 +306,6 @@
 					self._addTocounter({
 						product: name,
 						kcals: kcals,
-=======
-					var subTotal = qty * price;
-					var total = self._convertString( self.storage.getItem( self.total ) );
-					var sTotal = total + subTotal;
-					self.storage.setItem( self.total, sTotal );
-					self._addToCart({
-						product: name,
-						price: price,
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 						qty: qty
 					});
 					var shipping = self._convertString( self.storage.getItem( self.shippingRates ) );
@@ -521,11 +350,7 @@
 		
 		// Empties the session storage
 		
-<<<<<<< HEAD
 		_emptycounter: function() {
-=======
-		_emptyCart: function() {
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 			this.storage.clear();
 		},
 		
@@ -544,7 +369,6 @@
 		
 		/* Extract the numeric portion from a string
 		 * @param element Object the jQuery element that contains the relevant string
-<<<<<<< HEAD
 		 * @returns kcals String the numeric string
 		 */
 		
@@ -554,17 +378,6 @@
 			var text = element.text();
 			var kcals = text.replace( self.currencyString, "" ).replace( " ", "" );
 			return kcals;
-=======
-		 * @returns price String the numeric string
-		 */
-		
-		
-		_extractPrice: function( element ) {
-			var self = this;
-			var text = element.text();
-			var price = text.replace( self.currencyString, "" ).replace( " ", "" );
-			return price;
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 		},
 		
 		/* Converts a numeric string into a number
@@ -622,18 +435,12 @@
 		},
 		
 		
-<<<<<<< HEAD
 		/* Add an object to the counter as a JSON string
 		 * @param values Object the object to be added to the counter
-=======
-		/* Add an object to the cart as a JSON string
-		 * @param values Object the object to be added to the cart
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 		 * @returns void
 		 */
 		
 		
-<<<<<<< HEAD
 		_addTocounter: function( values ) {
 			var counter = this.storage.getItem( this.counterName );
 			
@@ -646,20 +453,6 @@
 		},
 		
 		/* Custom shipping rates calculation based on the total quantity of items in the counter
-=======
-		_addToCart: function( values ) {
-			var cart = this.storage.getItem( this.cartName );
-			
-			var cartObject = this._toJSONObject( cart );
-			var cartCopy = cartObject;
-			var items = cartCopy.items;
-			items.push( values );
-			
-			this.storage.setItem( this.cartName, this._toJSONString( cartCopy ) );
-		},
-		
-		/* Custom shipping rates calculation based on the total quantity of items in the cart
->>>>>>> 93541aff5f94a04699156ae48ca9498aeebabb62
 		 * @param qty Number the total quantity of items
 		 * @returns shipping Number the shipping rates
 		 */
